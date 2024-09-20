0ac1f1da01429cfbe95b09646429b45f45630f0c
Loic <panguslariss@gmail.com>
2024-09-20 11:33:01 +0100

    adding scrollreval librarie

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import Header from './layout/Header'
 import Footer from './layout/Footer'
 import social_links from "./data/social_links.json"
+
 function App() {
   return (
     <main className="bg-white relative isolate  pt-14  h-screen">
@@ -10,8 +11,8 @@ function App() {
       <Header/>
       <Outlet/>
       <Footer/>
-    <a href={`https://wa.me/${social_links[0].href}`} className="fixed w-10 bottom-1 right-1  md:w-20 md:bottom-10 md:right-10">
-      <img src="/logo_whatsapp.svg" className=" icon-whatsapp" alt="whatsapp" />
+    <a href={`https://wa.me/${social_links[0].href}`} className="fixed w-12 bottom-7 right-3  md:w-16 md:bottom-7 md:right-7">
+      <img src="/logo_whatsapp.svg"  alt="whatsapp" />
     </a>
     </main>
   )
