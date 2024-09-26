12a446481b85c68f21b9a82a58ed64ad82d27c1e
Loic <panguslariss@gmail.com>
2024-09-26 18:07:51 +0100

    element de colone pour la table des product

---
+++ src/components/table/ColumnHelper.tsx
@@ -0,0 +1,40 @@
+import { createColumnHelper } from "@tanstack/react-table"
+import {type_product} from "../../types/type_product"
+
+const ColumnHelper = ()=>{
+
+const columnHelper = createColumnHelper<type_product>()
+return [
+  columnHelper.accessor('_id', {
+    header:"N",
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor("title", {
+    header: "Nom",
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('category', {
+    header: "Category",
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('desc', {
+    header: "Description",
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('prize', {
+    header: "Prix",
+    footer: info => info.column.id,
+  }),
+  
+  columnHelper.accessor('in_stock', {
+    header: 'En stock',
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('img', {
+    header: 'Images',
+    footer: info => info.column.id,
+  })
+]
+}
+
+export default ColumnHelper
