32136e514ac52c0a3bb0a33ebc002eaa48604384
Loic <panguslariss@gmail.com>
2024-10-03 15:55:20 +0100

    change type type_fruits

--- src/types/type_product.ts
+++ src/types/type_product.ts
@@ -1,10 +1,16 @@
+import { DocumentData, DocumentReference, DocumentSnapshot, QuerySnapshot } from "firebase/firestore/lite"
+
 export type type_product = {
-    _id: string
+    id: string
     category: string
     title: string
     prize: number
-    img: string
+    img: string[]
     symbolprize:string,
     in_stock:boolean,
     desc:string
   } 
+
+  export type promiseGetAll =  Promise<QuerySnapshot<DocumentData, DocumentData>>
+  export type promiseGetOne =    Promise<DocumentSnapshot<any>>
+  export type promiseAddDocc = Promise<DocumentReference<any, DocumentData>>
