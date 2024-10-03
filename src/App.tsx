d71b63c2d437a51f932e1872ae0c894f4b169a19
Loic <panguslariss@gmail.com>
2024-10-03 15:51:47 +0100

    adding contrainte for show whatsapp logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from './layout/Header'
 import Footer from './layout/Footer'
 import social_links from "./data/social_links.json"
+import { useEffect, useState } from 'react'
 
 function App() {
+  const location = useLocation()
+  const [showLogoWhat,setter]=useState(true)
+  useEffect(()=>{
+  
+    if(location.pathname.includes("admin")) setter(false)
+      else setter(true)
+   
+  },[location.pathname])
   return (
     <main className="bg-white relative isolate  pt-14  h-full">
-        
       <Header/>
       <Outlet/>
       <Footer/>
-    <a href={`https://wa.me/${social_links[0].href}`} className="fixed w-12 bottom-7 right-3  md:w-16 md:bottom-7 md:right-7">
-      <img src="/logo_whatsapp.svg"  alt="whatsapp" />
-    </a>
+      {
+        showLogoWhat?(
+          <a href={`https://wa.me/${social_links[0].href}`} className="fixed w-12 bottom-7 right-3  md:w-16 md:bottom-7 md:right-7">
+          <img src="/logo_whatsapp.svg"  alt="whatsapp" />
+        </a>
+        ):(
+          null
+        )
+      }
+   
     </main>
   )
 }
