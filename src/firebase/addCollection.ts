b9319ebba91b8f4562452678973e216b15635c1b
Loic <panguslariss@gmail.com>
2024-10-03 15:50:26 +0100

    function for adding collection

--- src/firebase/addCollection.ts
+++ src/firebase/addCollection.ts
@@ -1,4 +1,13 @@
+import { promiseAddDocc } from "../types/type_product";
 import { db } from "./config";
 import { addDoc ,collection} from "firebase/firestore/lite";
 
- 
+ export const addCollection = async(collection_name:string,data:any):promiseAddDocc=>{
+   const collection_ref = collection(db,collection_name)
+
+   try {
+    return await addDoc(collection_ref,data)
+   } catch (error:any) {
+    return error
+   }
+ }
