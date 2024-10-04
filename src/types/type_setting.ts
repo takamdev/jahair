f18e755aa2dd18bd4dc874059d93cbf873a3a25a
Loic <panguslariss@gmail.com>
2024-10-04 20:40:58 +0100

    type setting

---
+++ src/types/type_setting.ts
@@ -0,0 +1,12 @@
+export type type_setting = {
+    
+        symbole_devise:string,
+        profile_admis:string,
+        admin_name:string,
+        faq:string,
+        privacy_policy:string,
+        terms_conditions:string,
+        about_us:string,
+        logo:string
+    
+}
