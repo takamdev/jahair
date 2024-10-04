cce8ad367a66e35973b2e662201f3e0adc0c01da
Loic <panguslariss@gmail.com>
2024-10-04 20:42:52 +0100

    setting state

--- src/store.tsx
+++ src/store.tsx
@@ -1,22 +1,28 @@
 import { create } from "zustand";
 import {type_product} from "./types/type_product"
 export type cart = type_product[]
+import {type_setting} from "./types/type_setting"
+import setting from "./data/setting.json"
 
-
-
-interface CartState {
+interface typeStore {
   Cart: cart
   addCart: (product: type_product) => void
   resetCart:(cart:cart) => void
-  token:string
-  setToken:(token:string) => void
+  product:cart
+  setProduct:(products:cart) => void
+  addProduct:(product:type_product)=>void
+  setting:type_setting
+  setSetting:(setting:type_setting)=>void
 }
-const useStore = create<CartState>((set) => ({
+const useStore = create<typeStore>((set) => ({
+    setSetting:(setting)=>set(()=>({setting:setting})),
+    setting:setting,
     Cart: [],
     addCart: (product) => set((state) => ({ Cart:[...state.Cart,{...product,qte:1}]})),// dans ce cas T est de type le type de Cart
     resetCart:(cart)=>set(()=>({Cart:cart})),
-    token:"",
-    setToken:(token)=>set(()=>({token:token}))
+    product:[],
+    setProduct:(products)=>set(()=>({product:products})),
+    addProduct:(product)=>set((state)=>({product:[...state.product,product]}))
   }))
 
 
