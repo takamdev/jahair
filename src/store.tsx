a362ffe88d2530bd9839bd5053199c13f8d1fa8e
Loic <panguslariss@gmail.com>
2024-10-12 08:40:55 +0100

    reconfiguration du usestore

--- src/store.tsx
+++ src/store.tsx
@@ -1,16 +1,16 @@
 import { create } from "zustand";
 import {type_product} from "./types/type_product"
-export type cart = type_product[]
+type cart =( type_product & {qte?:number})
 import {type_setting} from "./types/type_setting"
 import setting from "./data/setting.json"
 
 interface typeStore {
-  Cart: cart
-  addCart: (product: type_product) => void
-  resetCart:(cart:cart) => void
-  product:cart
-  setProduct:(products:cart) => void
-  addProduct:(product:type_product)=>void
+  Cart: cart[]
+  addCart: (product: cart) => void
+  resetCart:(cart:cart[]) => void
+  product:type_product[]
+  setProduct:(products:type_product[]) => void
+  addProduct:(product:cart)=>void
   setting:type_setting
   setSetting:(setting:type_setting)=>void
 }
@@ -18,7 +18,7 @@ const useStore = create<typeStore>((set) => ({
     setSetting:(setting)=>set(()=>({setting:setting})),
     setting:setting,
     Cart: [],
-    addCart: (product) => set((state) => ({ Cart:[...state.Cart,{...product,qte:1}]})),// dans ce cas T est de type le type de Cart
+    addCart: (product) => set((state) => ({ Cart:[...state.Cart,{...product,qte:(product.qte ? product.qte : 1)}]})),// dans ce cas T est de type le type de Cart
     resetCart:(cart)=>set(()=>({Cart:cart})),
     product:[],
     setProduct:(products)=>set(()=>({product:products})),
