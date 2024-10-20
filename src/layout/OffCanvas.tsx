78328d926c92b5e388dcb169d56aca36c9b3ee34
Loic <panguslariss@gmail.com>
2024-10-20 14:25:47 +0100

    ajout des produits dans le panier

--- src/layout/OffCanvas.tsx
+++ src/layout/OffCanvas.tsx
@@ -1,54 +1,146 @@
+import { AiFillDelete } from "react-icons/ai"; 
+import { CgShoppingCart } from "react-icons/cg"; 
 
-"use client";
-
+import { Table } from "flowbite-react";
 import {Drawer } from "flowbite-react";
+import useStore from "../store";
+import { BiMinus, BiPlus } from "react-icons/bi";
+const theme={
+  "root": {
+    "base": "fixed z-40 overflow-y-auto bg-white p-4 transition-transform dark:bg-gray-800",
+    "backdrop": "fixed inset-0 z-30 bg-gray-900/50 dark:bg-gray-900/80",
+    "edge": "bottom-16",
+    "position": {
+      "top": {
+        "on": "left-0 right-0 top-0 w-full transform-none",
+        "off": "left-0 right-0 top-0 w-full -translate-y-full"
+      },
+      "right": {
+        "on": "right-0 top-0 h-screen w-80 transform-none",
+        "off": "right-0 top-0 h-screen w-80 translate-x-full"
+      },
+      "bottom": {
+        "on": "bottom-0 left-0 right-0 w-full transform-none",
+        "off": "bottom-0 left-0 right-0 w-full translate-y-full"
+      },
+      "left": {
+        "on": "left-0 top-0 h-screen w-80 transform-none",
+        "off": "left-0 top-0 h-screen w-80 -translate-x-full"
+      }
+    }
+  },
+  "header": {
+    "inner": {
+      "closeButton": "absolute end-2.5 top-2.5 flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white",
+      "closeIcon": "h-4 w-4",
+      "titleIcon": "me-2.5 h-4 w-4",
+      "titleText": "mb-4 inline-flex items-center text-2xl ms-2  font-bold dark:text-gray-400"
+    },
+    "collapsed": {
+      "on": "hidden",
+      "off": "block"
+    }
+  },
+  "items": {
+    "base": ""
+  }
+}
 
 export function OffCanvasCart({isOpen,setIsOpen}:{setIsOpen(value:boolean):void,isOpen:boolean}) {
+ const Cart = useStore(state=>state.Cart)
+ const resetCart = useStore(state=>state.resetCart)
+const handleClose = () => setIsOpen(false);
+const setting = useStore(state=>state.setting)
 
-  const handleClose = () => setIsOpen(false);
 
+const removeQte = (item:typeof Cart[0])=>{
+  if(item.qte!>1){
+    const newCart = Cart.map(element=>{
+      return  element.id ===item.id ? {...element,qte:element.qte! - 1}:element
+     })
+     resetCart(newCart)
+  }
+ 
+}
+const addQte =(item:typeof Cart[0])=>{
+  if(item.qte! <5){
+    const newCart = Cart.map(element=>{
+      return  element.id ===item.id ? {...element,qte:element.qte! + 1}:element
+     })
+     resetCart(newCart)
+  
+  }
+
+}
+
+const deleteProd = (id:string)=>{
+  const newCart = Cart.filter(element=>element.id!==id)
+   resetCart(newCart)
+}
   return (
     <>
-      <Drawer open={isOpen} onClose={handleClose} position="right" className="w-full md:w-96 lg:w-[500px] z-[1000]">
-        <Drawer.Header title="Drawer" />
+      <Drawer theme={theme}  open={isOpen} onClose={handleClose} position="right" className="w-full md:w-96 lg:w-[500px] z-[1000]">
+        <Drawer.Header  titleText="text-2xl" title="Panier" titleIcon={()=><CgShoppingCart className="me-2 scale-150" />} />
         <Drawer.Items>
-          <p className="mb-6 text-sm text-gray-500 dark:text-gray-400">
-            Supercharge your hiring by taking advantage of our&nbsp;
-            <a href="#" className="text-cyan-600 underline hover:no-underline dark:text-cyan-500">
-              limited-time sale
-            </a>
-            &nbsp;for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design
-            job board.
-          </p>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-            <a
-              href="#"
-              className="rounded-lg border border-gray-200 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-cyan-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-            >
-              Learn more
-            </a>
-            <a
-              href="#"
-              className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-            >
-              Get access&nbsp;
-              <svg
-                className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-          </div>
+          {
+            Cart.length===0 ?(
+              <p className="text-center text-2xl mt-5">Votre panier est vide</p>
+            ):(
+              <div className="h-full">
+                <section className="overflow-x-auto mt-5">
+                <Table>
+                  <Table.Head>
+                  <Table.HeadCell>Image</Table.HeadCell>
+                    <Table.HeadCell>Nom</Table.HeadCell>
+                    <Table.HeadCell>Prix</Table.HeadCell>
+                    <Table.HeadCell>qte</Table.HeadCell>
+                    <Table.HeadCell>action</Table.HeadCell>
+    
+                  </Table.Head>
+                  <Table.Body className="divide-y">
+                    {
+                      Cart.map(item=>{
+                        return (
+                          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                          <Table.Cell className="ms-o">
+                            <img src={item.img[0]} alt="toff"  />
+                          </Table.Cell>
+                          <Table.Cell>{item.title}</Table.Cell>
+                          <Table.Cell>{setting.symbole_devise==="$"?setting.symbole_devise +item.prize:item.prize+setting.symbole_devise}</Table.Cell>
+                          <Table.Cell>
+                        
+                            <p className="flex gap-2 items-center">
+                            <BiMinus className="cursor-pointer" onClick={()=>removeQte(item)} /> 
+                            {item.qte}
+                            <BiPlus className="cursor-pointer" onClick={()=>addQte(item)} />
+                            </p>
+                          </Table.Cell>
+                          <Table.Cell>
+                            <AiFillDelete onClick={()=>{deleteProd(item.id)}} className="text-red-600 cursor-pointer" />
+                          </Table.Cell>
+                        
+                        </Table.Row>
+                        )
+                      })
+                    }
+                  
+                  
+                  </Table.Body>
+                </Table>
+                
+                
+              
+                </section>
+
+                <p className="text-3xl absolute bottom-24 right-10  "><span className="font-bold">Total </span>:
+                  {setting.symbole_devise==="$"?setting.symbole_devise +Cart.reduce((acc,item)=>acc+(item.prize*item.qte!),0):Cart.reduce((acc,item)=>acc+(item.prize*item.qte!),0)+setting.symbole_devise}
+                
+                  </p>
+                  <a className="btn absolute bottom-10 lg:left-20 py-2 font-semibold text-lg px-24" href="">Passer au paiement</a>
+              </div>
+            )
+          }
+         
         </Drawer.Items>
       </Drawer>
     </>
