570920815bfa245ab36239ac36e813871f900716
Loic <panguslariss@gmail.com>
2024-11-15 11:35:14 +0100

    connection de la gallery a firebase pour le controlle

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,22 +1,16 @@
 import  {useEffect, useRef, useState } from "react";
 
-export function FeaturedImageGallery() {
-  const data = [
-    {
-      imgelink:
-        "/img/coup1.webp",
-    },
-    {
-      imgelink:
-        "/img/coup2.webp",
-    },
-    {
-      imgelink:
-        "/img/coup3.webp",
-    }
-  ];
 
-  const [active,setActive]=useState<string>(data[0].imgelink) 
+export function FeaturedImageGallery({img}:{img:string[]}) {
+  
+  const [data,setData]=useState<string[]|null>(null)
+  const [active,setActive]=useState<string>(img[0]) 
+
+  useEffect(()=>{
+    setData(img)
+  },[])
+
+
  
 
  
@@ -24,13 +18,13 @@ export function FeaturedImageGallery() {
     <div className="grid gap-4">
       <div>
         <img
-          className="h-96 w-full max-w-full rounded-lg object-cover object-center md:h-[480px]"
+          className=" w-full max-w-full rounded-lg  object-center md:h-[480px]"
           src={active}
           alt=""
         />
       </div>
       <div className="grid grid-cols-5 gap-2">
-        {data.map(({ imgelink }, index) => (
+        {data?.map(( imgelink , index) => (
           <div key={index} className="relative">
             <img
               onClick={() => setActive(imgelink)}
@@ -48,32 +42,20 @@ export function FeaturedImageGallery() {
 
 
 
-export function FeaturedVideoGallery() {
-  const data = [
-    {
-      videolink:
-        "/video/v1_service.mp4",
-    },
-    {
-      videolink:
-        "/video/v2_service.mp4",
-    },
-    {
-      videolink:
-       "/video/v3_service.mp4"
-    }
-  ];
+export function FeaturedVideoGallery({videos}:{videos:string[]}) {
+  const [data,setData]=useState<string[]|null>(null)
+  const [active,setActive]=useState<string>(videos[0]) 
+
+  useEffect(()=>{
+    setData(videos)
+  },[])
   
-  const [active,setActive]=useState<string>(data[0].videolink) 
   const videoRef = useRef<HTMLVideoElement | null>(null)
   const [matchMedia,setMatchMedia] = useState<boolean>(window.innerWidth>1000)
   useEffect(()=>{
-    if(videoRef.current!==null){
-      videoRef.current.playbackRate=2
-
-
-
-    }
+   
+    videoRef.current!.playbackRate=2
+    
     const mediaQuery = window.matchMedia('(min-width: 1000px)');
     mediaQuery.addListener((e)=>{
       setMatchMedia(e.matches)
@@ -106,7 +88,7 @@ export function FeaturedVideoGallery() {
         />
       </div>
       <div className="grid grid-cols-5 gap-2">
-        {data.map(({ videolink }, index) => (
+        {data?.map((videolink, index) => (
           <div key={index} className="relative">
             <video
               onClick={() => setActive(videolink)}
